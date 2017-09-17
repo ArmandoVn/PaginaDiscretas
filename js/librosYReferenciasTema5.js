1f6535206845adba28fc23e46da98947f9c0d9f3
Armando VN <armandoeloriginal@live.com.mx>
2017-09-17 03:25:55 -0500

    Se agregan tema4 y 5, se corrigen errores de la version anterior, se agrega footer a la pagina, se agrega icono

---
+++ js/librosYReferenciasTema5.js
@@ -0,0 +1,40 @@
+
+var libros = ["BERNAN, Kenneth,PAUL,Jerome . \"Algorithms: Sequential, Parallel, and Distributed.\"",
+"EPP,Susanne S. \"Discrete mathematics with applications\"",
+"GRASSMAN,Winfried,TREMBLAY,Jean-Paul. \"Matemática discreta y lógica: Una perspectiva desde la Ciencia de la COmputación\"",
+"JOHNSONBAUGH,Richard.\"Discrete Mathematics\"",
+"KOLMAN,Bernard. \"Discrete Mathematical Structures\"",
+"LIU,C.L. \"Elementos de matmaticas discretas\"",
+"ROSEN,Kenneth. \"Matematicas discretas y sus aplicaciones.\"",
+"TREMBLAY,Jean-Paul.\"Matematicas Discretas con aplicacion a las ciencias de la computacion\"",
+"VEERARAJAN,t. \"Matematicas discretas con teoria de graficas y combinatoria\""];
+
+var ubicacion = ["Facultad de Ingeniería",
+"Centro de Ciencias Geonómicas",
+"No Disponible",
+"Instituto de Fisica",
+"Facultad de Estudios Superiores Aragón",
+"Biblioteca Central,Facultad de Contaduria y Adminitracion, Facultad de Ingenieria: Posgrado",
+"Facultad de Ciencias, Facultad de Ingenieria: Enrique Rivero Borell, Facultad de Estudios Superiores Cuatitulan 1",
+"FES: Aragon, Facultad de COntaduria y Administracion, FES: Cuatitlan 1, Facultad de Ciencias, Facultad de Ingenieria, Facultad de Ingenieria: Enrique Rivero Borrell, Instituo de Investigaciones Economicas, Instituto de Matematicas.",
+"FES Acatlan, Facultad de Ingenieria, Biblioteca Central, Facultad de Ciencias, FES Cuatitlan 1, Facultad de Ingenieria: Enrique Rivero Borrell."];
+
+var clasificacion = ["QA76.9A43 B467",
+"QA39.2 E66 2011",
+"No Disponible",
+"QA39.2 J64 2009",
+"QA76.9 K64 2009",
+"QA 164 L5818",
+"QA39.2 R6618 2004",
+"QA39.2T44 1996",
+"QA248 V4418"];
+
+var libLinea = ["",
+"",
+"http://www.mediafire.com/file/wbw2ehb6k43z6wf/2+Matematica_discreta_y_logica-W.K.Grassmann.rar",
+"",
+"",
+"",
+"",
+"",
+""];
